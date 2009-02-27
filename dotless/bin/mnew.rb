b4f2b8a4fc8c82699578d6859a999020041b5787
Andrei Dragomir <adragomir@gmail.com>
2009-02-27 23:03:18 +0200

    update

--- dotless/bin/mnew.rb
+++ dotless/bin/mnew.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/env ruby
+if (ARGV.length < 1)
+	puts "Error: show, do"
+	exit
+end
+command = ARGV[0]
 # get the list of files, separated by spaces
 files = `find . -type f | p4 -x- have 2>&1`
 collect = []
@@ -8,6 +13,12 @@ files.split("\n").each { |l|
 	collect.push(l)
 }
 # open for edit
-system("p4 add #{collect.join(' ')}")
+if (command == "show")
+	puts collect
+end
+
+if (command == "do")
+	system("p4 add #{collect.join(' ')}")
+end
 exit
 
